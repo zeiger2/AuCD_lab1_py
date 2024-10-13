a78bfca8d8754e42bae932ab35d683ea9fa8b49e
zeiger2 <zverevart@inbox.ru>
2024-10-13 22:34:00 +0300

    2

--- InsertionSort/InsertionSort/InsertionSort.py
+++ InsertionSort/InsertionSort/InsertionSort.py
@@ -1,6 +1,21 @@
-from ast import main
+from random import *
 
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
 
-123
+def RandomArr(kol,_seed):
+    seed(_seed)
+    A=[randint(0,50) for _ in range(kol)]
+    return A
 
-main
+A=[]
+A=RandomArr(15,5)
+print(A)
+#print(insertion_sort(A))
